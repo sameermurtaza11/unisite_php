3d8faebc0a8866e09330ce77ea7e4d82223fcb7c
Sameer Murtaza <106397102+sameermurtaza11@users.noreply.github.com>
2025-07-24 15:43:45 +0500

    Delete unisite_php directory

--- unisite_php/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-$content_file = isset($_GET['content_file']) ? $_GET['content_file'] : 'pages/home.php';
-$title = 'Home - KINPOE';
-include 'layouts/main.php';
-?>
--- unisite_php/pages/home.php
+++
@@ -1,6 +0,0 @@
-<?php include 'partials/hero.php'; ?>
-<?php include 'partials/featured-programs.php'; ?>
-<?php include 'partials/latest-news.php'; ?>
-<?php include 'partials/research-preview.php'; ?>
-<?php include 'partials/gallery-preview.php'; ?>
-<?php include 'partials/cta.php'; ?>
